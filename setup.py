aef7ba520886353d7294c2a10a3a45d88f0ebcec
ChungSeop <tooha289@naver.com>
2023-09-20 10:02:15 +0900

    setup.py 추가

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DataAnalysisLibrary",
+    version="0.1",
+    description="The goal of the 'DataAnalysisLibrary' is to provide helpful libraries for performing data analysis.",
+    author="Shin Chungseop",
+    author_email="tooha289@naver.com",
+    packages=find_packages(),  # 현재 디렉토리에서 모든 패키지를 찾습니다.
+)
