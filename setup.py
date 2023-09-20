32251fb9ddef00836fd0e82ca711d26d559b0d9b
ChungSeop <tooha289@naver.com>
2023-09-20 14:37:03 +0900

    setup 파일 수정

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
     description="The goal of the 'DataAnalysisLibrary' is to provide helpful libraries for performing data analysis.",
     author="Shin Chungseop",
     author_email="tooha289@naver.com",
+    url="https://github.com/tooha289/DataAnalysisLibrary",
     packages=find_packages(),  # 현재 디렉토리에서 모든 패키지를 찾습니다.
 )
