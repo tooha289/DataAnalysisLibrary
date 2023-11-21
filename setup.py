97972ef3a2bbf4a64f7358f1420eb5f0ee13a19e
ChungSeop <tooha289@naver.com>
2023-11-21 11:09:06 +0900

    SETUP 수정

--- setup.py
+++ setup.py
@@ -2,10 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="DataAnalysisLibrary",
-    version="0.2.8",
+    version="0.3.0",
     description="The goal of the 'DataAnalysisLibrary' is to provide helpful libraries for performing data analysis.",
     author="Shin Chungseop",
     author_email="tooha289@naver.com",
     url="https://github.com/tooha289/DataAnalysisLibrary",
     packages=find_packages(),
+    install_requires=[
+        'matplotlib',
+        'seaborn',
+        'numpy',
+        'pandas',
+        'statsmodels',
+        'scikit-learn'
+    ]
 )
